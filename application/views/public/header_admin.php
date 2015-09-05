6a98a466c8b39972c2dba3de7e5dc66925d887a3
decradish <liukangning@gmail.com>
2015-09-05 17:11:53 +0800

    add CI

---
+++ application/views/public/header_admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>亿众官网后台</title>
+
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="shortcut icon" href="/public/img/favicon.ico" />
+	<link rel="stylesheet" type="text/css" media="all" href="/public/css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="/public/css/admin.css" />
+
+	<script type="text/javascript" src="/public/js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="/public/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="wrapper">
+	<h2>
+		亿众官网后台
+		<a class="btn btn-info" href="/admin/">首页</a>
+		<a class="btn btn-success" href="/admin/add">新建</a>
+	</h2>
